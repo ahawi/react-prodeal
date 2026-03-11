6d6adf846515a14c17ea77f1e793c5ddbf38038e
ahawi <nastya.murlla@gmail.com>
2026-03-11 20:06:47 +0300

    start main page

---
+++ src/pages/main/ui/MainPage.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@/shared/ui/typography"
+
+const MainPage = () => {
+  return (
+    <>
+      <Typography variant='h1'>Заголовок 1</Typography>
+    </>
+  )
+}
+
+export default MainPage
