e7ec2729aabc5659c99d3ae576454e65a26f9e75
ahawi <nastya.murlla@gmail.com>
2026-03-11 17:07:24 +0300

    fix closeicon

--- src/shared/ui/Icons/CloseIcon.tsx
+++ src/shared/ui/Icons/CloseIcon.tsx
@@ -5,9 +5,9 @@ export const CloseIcon = (props: SVGProps<SVGSVGElement>) => (
     <path
       d='M0.75 9.48833L5.11917 5.11917M9.48833 0.75L5.11833 5.11917M5.11833 5.11917L0.75 0.75M5.11917 5.11917L9.48833 9.48833'
       stroke='#BFC1C7'
-      stroke-width='1.5'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     />
   </svg>
 )
