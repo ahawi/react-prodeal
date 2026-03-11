b7c4c74a32979ffe398e567e741fecd1a3afeab0
ahawi <nastya.murlla@gmail.com>
2026-03-11 14:05:53 +0300

    added closeicon

---
+++ src/shared/ui/Icons/CloseIcon.tsx
@@ -0,0 +1,13 @@
+import type {SVGProps} from 'react'
+
+export const CloseIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={11} height={11} viewBox='0 0 11 11' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path
+      d='M0.75 9.48833L5.11917 5.11917M9.48833 0.75L5.11833 5.11917M5.11833 5.11917L0.75 0.75M5.11917 5.11917L9.48833 9.48833'
+      stroke='#BFC1C7'
+      stroke-width='1.5'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    />
+  </svg>
+)
