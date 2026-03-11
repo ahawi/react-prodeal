1ffe1fe64d32ab96fc9f09d45fb7358b174811fa
ahawi <nastya.murlla@gmail.com>
2026-03-11 14:06:06 +0300

    added open and close eye icon

---
+++ src/shared/ui/Icons/CloseEyeIcon.tsx
@@ -0,0 +1,20 @@
+import type {SVGProps} from 'react'
+
+export const CloseEyeIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg width={20} height={20} viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg' {...props}>
+    <path
+      d='M0.75 0.75L18.75 18.75M8.25 8.427C7.91306 8.80851 7.73426 9.30413 7.75003 9.81288C7.76579 10.3216 7.97494 10.8052 8.33485 11.1651C8.69477 11.5251 9.17837 11.7342 9.68712 11.75C10.1959 11.7657 10.6915 11.5869 11.073 11.25'
+      stroke='#2C2D2E'
+      stroke-width='1.5'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    />
+    <path
+      d='M5.112 5.311C3.43 6.49 2.029 8.17 0.75 9.75C2.639 12.741 6.032 15.75 9.75 15.75C11.3 15.75 12.793 15.227 14.145 14.4M9.75 3.75C13.758 3.75 16.451 6.908 18.75 9.75C18.4205 10.2708 18.0605 10.7716 17.672 11.25'
+      stroke='#2C2D2E'
+      stroke-width='1.5'
+      stroke-linecap='round'
+      stroke-linejoin='round'
+    />
+  </svg>
+)
