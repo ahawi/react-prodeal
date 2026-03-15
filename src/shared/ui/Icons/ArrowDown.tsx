b7ca686ae0c46349ea31cc6673a57c51287715b7
ahawi <nastya.murlla@gmail.com>
2026-03-15 18:54:29 +0300

    added arrow down icon

---
+++ src/shared/ui/Icons/ArrowDown.tsx
@@ -0,0 +1,26 @@
+import type { SVGProps } from 'react'
+
+export const ArrowDownIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={16}
+    height={16}
+    viewBox="0 0 16 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <g clipPath="url(#clip0_13620_12677)">
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M2.96967 5.46967C3.26257 5.17678 3.73744 5.17678 4.03033 5.46967L7.8232 9.26256C7.92083 9.36019 8.07912 9.36019 8.17675 9.26256L11.9697 5.46967C12.2626 5.17678 12.7374 5.17678 13.0303 5.46967C13.3232 5.76257 13.3232 6.23744 13.0303 6.53033L9.23741 10.3232C8.55399 11.0066 7.44595 11.0066 6.76254 10.3232L2.96967 6.53033C2.67678 6.23743 2.67678 5.76256 2.96967 5.46967Z"
+        fill="#ADADAD"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_13620_12677">
+        <rect width={16} height={16} fill="white" />
+      </clipPath>
+    </defs>
+  </svg>
+)
