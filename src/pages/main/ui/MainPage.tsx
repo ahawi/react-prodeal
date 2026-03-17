45f896e8c02d5b543eeaff8dc77c386f8dab2ba2
ahawi <nastya.murlla@gmail.com>
2026-03-17 12:45:06 +0300

    moved assets from app to shared

--- src/pages/main/ui/MainPage.tsx
+++ src/pages/main/ui/MainPage.tsx
@@ -1,11 +1,25 @@
 import { Header } from '@/widgets/header'
 import { Hero } from '@/widgets/hero'
+import { FeatureCards } from '@/widgets/feature-cards'
+import { About } from '@/widgets/about'
+import { HowItWorks } from '@/widgets/how-it-works'
+import { Calculator } from '@/widgets/calculator'
+import { Reviews } from '@/widgets/reviews'
+import { Consultation } from '@/widgets/consultation'
+import { Footer } from '@/widgets/footer'
 
 const MainPage = () => {
   return (
     <>
       <Header />
       <Hero />
+      <FeatureCards />
+      <About />
+      <HowItWorks />
+      <Calculator />
+      <Reviews />
+      <Consultation />
+      <Footer />
     </>
   )
 }
