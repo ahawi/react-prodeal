255cb9bfc5ecfdd2060caf56901801ad81fa8b4f
ahawi <nastya.murlla@gmail.com>
2026-03-24 14:03:38 +0300

    added footer for auth

--- src/widgets/footer/ui/Footer.tsx
+++ src/widgets/footer/ui/Footer.tsx
@@ -1,6 +1,8 @@
 import { SocialIcon } from '@/shared/ui/icons/SocialIcon'
 import styles from './Footer.module.scss'
 import Logo from '@/shared/assets/images/logo-grey.svg'
+import { Link, useLocation } from 'react-router'
+import { Typography } from '@/shared/ui/typography'
 
 const mainLinks = [
   { title: 'Для продавца', href: '#' },
@@ -16,68 +18,89 @@ const socials = [
 ]
 
 const Footer = () => {
+  const location = useLocation()
+  const isLogin =
+    location.pathname.includes('login') ||
+    location.pathname.includes('register')
+
   return (
     <footer className={styles.footer}>
-      <div className={`${styles.footer__inner} container`}>
-        <ul className={styles.footer__privacy__list}>
-          <li className={styles.footer__item}>
-            <a href="#" className={styles.footer__linkLogo}>
-              <img
-                className={styles.footer__logo}
-                src={Logo}
-                alt="Prodeal"
-                width="167"
-                height="26"
-                loading="lazy"
-              />
-            </a>
-          </li>
-
-          <li className={styles.footer__item}>
-            <p className={styles.footer__coryright}>
-              © <time dateTime="2022">2022</time> PRODEAL <br />
-              Все права защищены
-            </p>
-          </li>
-
-          <li className={styles.footer__item}>
-            <a href="#" className={styles.footer__link}>
-              Политика конфиденциальности
-            </a>
-          </li>
-        </ul>
-
-        <ul className={styles.footer__list}>
-          {mainLinks.map((link) => (
-            <li className={styles.footer__item} key={link.title}>
-              <a href={link.href} className={styles.footer__link}>
-                {link.title}
+      {!isLogin && (
+        <div className={`${styles.footer__inner} container`}>
+          <ul className={styles.footer__privacy__list}>
+            <li className={styles.footer__item}>
+              <a href="#" className={styles.footer__linkLogo}>
+                <img
+                  className={styles.footer__logo}
+                  src={Logo}
+                  alt="Prodeal"
+                  width="167"
+                  height="26"
+                  loading="lazy"
+                />
               </a>
             </li>
-          ))}
-        </ul>
 
-        <div className={styles.footer__actions}>
-          <div className={`${styles.footer__soc1als} ${styles.soc1als}`}>
-            <ul className={styles.soc1als__list}>
-              {socials.map((social) => (
-                <li className={styles.soc1als__item} key={social.name}>
-                  <a href={social.href} className={styles.soc1als__link}>
-                    <span className="visually-hidden">{social.label}</span>
-                    <SocialIcon name={social.name} />
-                  </a>
-                </li>
-              ))}
-            </ul>
+            <li className={styles.footer__item}>
+              <p className={styles.footer__coryright}>
+                © <time dateTime="2022">2022</time> PRODEAL <br />
+                Все права защищены
+              </p>
+            </li>
+
+            <li className={styles.footer__item}>
+              <a href="#" className={styles.footer__link}>
+                Политика конфиденциальности
+              </a>
+            </li>
+          </ul>
+
+          <ul className={styles.footer__list}>
+            {mainLinks.map((link) => (
+              <li className={styles.footer__item} key={link.title}>
+                <a href={link.href} className={styles.footer__link}>
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+
+          <div className={styles.footer__actions}>
+            <div className={`${styles.footer__soc1als} ${styles.soc1als}`}>
+              <ul className={styles.soc1als__list}>
+                {socials.map((social) => (
+                  <li className={styles.soc1als__item} key={social.name}>
+                    <a href={social.href} className={styles.soc1als__link}>
+                      <span className="visually-hidden">{social.label}</span>
+                      <SocialIcon name={social.name} />
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <a className={styles.footer__link} href="tel:79923454087">
+              +7 992 345-40-87
+            </a>
+            <a href="#" className={styles.footer__link}>
+              Создать сделку
+            </a>
           </div>
-          <a className={styles.footer__link} href="tel:79923454087">
-            +7 992 345-40-87
-          </a>
-          <a href="#" className={styles.footer__link}>
-            Создать сделку
-          </a>
         </div>
-      </div>
+      )}
+
+      {isLogin && (
+        <div className={`${styles.loginInner} container`}>
+          <div className={styles.loginLeft}>
+            <Typography variant="text">© 2022 PRO-DEAL</Typography>
+            <Typography variant="text">Все права защищены</Typography>
+          </div>
+          <div className={styles.loginRight}>
+            <Link to="/" className={styles.footer__link}>
+              <Typography variant="text">Помощь</Typography>
+            </Link>
+          </div>
+        </div>
+      )}
     </footer>
   )
 }
