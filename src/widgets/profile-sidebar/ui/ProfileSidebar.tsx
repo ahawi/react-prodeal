60de5f2292a3f19abdd5a31d5a5af7089500a2ac
ahawi <nastya.murlla@gmail.com>
2026-03-25 20:35:39 +0300

    added profile sidebar

---
+++ src/widgets/profile-sidebar/ui/ProfileSidebar.tsx
@@ -0,0 +1,44 @@
+import { Link } from 'react-router'
+import styles from './ProfileSidebar.module.scss'
+import profileIcon from '@/shared/assets/images/folder.svg'
+import dealsIcon from '@/shared/assets/images/wallet.svg'
+import { Typography } from '@/shared/ui/typography'
+import { LogoutIcon } from '@/shared/ui/icons/LogoutIcon'
+import { useAppDispatch } from '@/app/hooks/useAppDispatch'
+import { logoutUser } from '@/features/auth/model/authThunk'
+
+const ProfileSidebar = () => {
+  const dispatch = useAppDispatch()
+
+  const handleLogout = () => {
+    dispatch(logoutUser())
+  }
+  return (
+    <aside className={styles.sidebar}>
+      <nav className={styles.menu}>
+        <Link to="/profile" className={`${styles.menuItem} ${styles.active}`}>
+          <img src={profileIcon} alt="folder" className={styles.menuIcon} />
+          <Typography variant="text">Профиль</Typography>
+        </Link>
+
+        <Link to="/deals" className={styles.menuItem}>
+          <img src={dealsIcon} alt="folder" className={styles.menuIcon} />
+          <Typography variant="text">Сделки</Typography>
+        </Link>
+      </nav>
+
+      <button
+        type="button"
+        className={styles.logoutButton}
+        onClick={handleLogout}
+      >
+        <div className={styles.iconWrapper}>
+          <LogoutIcon className={styles.logoutIcon} />
+        </div>
+        <Typography variant="text">Выйти</Typography>
+      </button>
+    </aside>
+  )
+}
+
+export { ProfileSidebar }
