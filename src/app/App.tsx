c18a05c6e3e54c9395c888a9941beb48a7f04166
ahawi <nastya.murlla@gmail.com>
2026-03-25 12:01:17 +0300

    added load from storage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,18 @@ import { router } from './router'
 import { useAppDispatch } from './hooks/useAppDispatch'
 import { useEffect } from 'react'
 import { fetchProfile } from '@/features/auth/model/authThunk'
+import { loadUserFromStorage } from '@/features/auth/model/authSlice'
+import { authStorage } from '@/features/auth/model/authStorage'
 
 function App() {
   const dispatch = useAppDispatch()
 
   useEffect(() => {
-    dispatch(fetchProfile())
+    dispatch(loadUserFromStorage())
+
+    if (authStorage.read()) dispatch(fetchProfile())
   }, [dispatch])
+
   return <RouterProvider router={router} />
 }
 
