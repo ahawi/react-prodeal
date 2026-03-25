24df9ebfb7329bfa2a83eea54e65b5189494119a
ahawi <nastya.murlla@gmail.com>
2026-03-25 20:35:59 +0300

    added logout icon

---
+++ src/shared/ui/Icons/LogoutIcon.tsx
@@ -0,0 +1,34 @@
+import type { SVGProps } from 'react'
+
+export const LogoutIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={32}
+    height={32}
+    viewBox="0 0 32 32"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M20.6799 19.4286L23.9999 16L20.6799 12.5714"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M23.9999 16H13.7142"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path
+      d="M20.5714 8L11.4285 8.00257C10.1664 8.00356 9.14346 9.02681 9.14282 10.2889V21.7111C9.14282 22.9734 10.1662 23.9968 11.4285 23.9968H20.6805"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+)
