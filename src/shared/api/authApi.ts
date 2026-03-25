e29467e11472d6e5c765fa346c5d34733a67f6bb
ahawi <nastya.murlla@gmail.com>
2026-03-25 11:56:42 +0300

    fixed types

--- src/shared/api/authApi.ts
+++ src/shared/api/authApi.ts
@@ -1,3 +1,4 @@
+import type { User } from '@/features/auth/model/authThunk'
 import axios from 'axios'
 
 export const api = axios.create({
@@ -19,24 +20,27 @@ interface RegisterData {
   isAgree: boolean
 }
 
+interface AuthResponse {
+  user: User
+}
+
 export const authApi = {
   login: async (data: LoginData) => {
-    const response = await api.post('/api/auth/login', data)
+    const response = await api.post<AuthResponse>('/api/auth/login', data)
     return response.data
   },
 
   register: async (data: RegisterData) => {
-    const response = await api.post('/api/auth/register', data)
+    const response = await api.post<AuthResponse>('/api/auth/register', data)
     return response.data
   },
 
   getProfile: async () => {
-    const response = await api.get('/api/profile')
+    const response = await api.get<User>('/api/profile')
     return response.data
   },
 
   logout: async () => {
-    const response = await api.post('/api/logout')
-    return response.data
+    await api.post('/api/logout')
   },
 }
